c31008acd3cc914db3ab5d48c74e3a75d1a7a963
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-10-31 19:58:30 +0800

    Update Prolog Challenges/ebbyA258.pl

---
+++ Prolog Challenges/ebbyA258.pl
@@ -0,0 +1,22 @@
+character(joey).
+character(ross).
+character(rachel).
+character(phoebe).
+character(monica).
+character(chandler).
+likes(ross,rachel). /* This means ross likes rachel */
+likes(rachel,ross).
+likes(monica,chandler).
+likes(chandler,monica).
+likes(chandler,phoebe).
+likes(phoebe,joey).
+likes(monica,joey).
+likes(joey,rachel).
+
+dating(X, Y) :- /* These are rules. */
+likes(X, Y),
+likes(Y, X).
+
+attraction(X, Y) :-
+likes(X, Y);
+likes(Y, X).
